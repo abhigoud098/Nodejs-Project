4046fb59fad871129701fbff2620f11267f30e39
Abhishek <abhigoud198484@gmail.com>
2025-08-15 15:31:21 +0530

    Add some Functionality

--- server.js
+++ server.js
@@ -17,38 +17,65 @@ const userSchema = new mongoose.Schema({
 
 const dbWaleBhiya = mongoose.model("user", userSchema, "information");
 
-app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.static("views"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.sendFile("/login.html", { root: "public" });
+  res.render("login");
 });
 app.get("/signup", (req, res) => {
-  res.sendFile("/signup.html", { root: "public" });
+  res.render("signup");
 });
 
 app.post("/formsubmit", (req, res) => {
   const data = new dbWaleBhiya(req.body);
   data.save();
-  res.sendFile("/gone.html", {root: 'public'});
+  res.render("gone");
 });
+
 app.post("/login", async (req, res) => {
   const data = req.body;
-  const user = await dbWaleBhiya.find({email :  data.email})
-  console.log(user)
+  const user = await dbWaleBhiya.find({ email: data.email });
+  console.log(user);
   //Authentication code
-  if (user.length > 0){
+  if (user.length > 0) {
     //authorisation code
-    if (data.pass == user[0].pass && data.email == user[0].email){
-        res.send(`aap a skte hain janab ji aayiye aap ka intjaar tha. aapka naam ${user[0].name} hai na? welcome! ☺️`);
+    if (data.pass == user[0].pass && data.email == user[0].email) {
+      res.render("welcome", {name: user[0].name});
     } else {
-        res.send(`Sir ji ${user[0].pass} galat password hai app kon hai 😡`);
+      res.send(`Sir ji ${user[0].pass} galat password hai app kon hai 😡`);
     }
-  } else{
-    res.send('user not exist')
+  } else {
+    res.send("user not exist");
   }
-
 });
+
+//Delete your data
+app.post("/delet_data", async (req, res) => {
+  const data = await req.body;
+  console.log(data.username);  
+  const user = await dbWaleBhiya.find({username: data.username}) //Find to match the pass
+  if (user[0].pass == data.pass){  //Pass. match
+    await dbWaleBhiya.findOneAndDelete({username: data.username}) // Find again and Delete it
+    res.send('account deleted sorry to see you go')
+  } else {
+    res.send('err aagya')
+  }
+});
+
+app.post("/update_data", async (req, res) => {
+  const data = await req.body;
+  const user = dbWaleBhiya.find({email: data.email}) //Find to match the pass
+  if (user[0].pass == data.pass){  //Pass. match
+    dbWaleBhiya.findOneAndUpdate({username: data.username}) // Find again and Delete it
+    res.send("Data is Updated....!")
+  }else {
+    res.send("Bhiya kon hai App.....!")
+  }
+});
+
+
 
 app.listen(3000, () => {
   console.log("server is running");
